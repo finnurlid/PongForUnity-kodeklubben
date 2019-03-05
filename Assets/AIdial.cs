028f5363b87808f4b31ab13a1b3e313f76f1b5b1
Finn Urlid <finnurlid@fesshompa.no>
2019-03-05 22:36:09 +0100

    First commit

---
+++ Assets/AIdial.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIdial : MonoBehaviour
+{
+    public float boundY = 2.25f;
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = GameObject.Find("Ball").transform.position;
+
+        if (pos.y > boundY)
+        {
+            pos.y = boundY;
+        }
+        else if (pos.y < -boundY)
+        {
+            pos.y = -boundY;
+        }
+
+        pos.x = -4.0f;
+
+        transform.position = pos;
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Pong for Unity 2018.3
+
+Kodeklubben i Salangen har laget klassikeren Pong i Scratch. Dette er samme spillet laget i spillmotoren Unity.
+
+Last ned. Pakk ut. Åpne i Unity.
+
+*Have fun, go mad!
+Finn*
